70999bed94062ed9d2a97e4ad8358eb4c0b40878
Alex Eckhart <eckhartalex@gmail.com>
2022-12-03 17:18:39 -0700

    Clean up day1.

--- common.rkt
+++ common.rkt
@@ -1,1 +1,16 @@
 #lang racket
+
+(provide line-split block-split exe-day)
+
+(define (line-split str)
+  (string-split str "\n"))
+
+(define (block-split str)
+  (map line-split (string-split str "\n\n")))
+
+(define (exe-day day-func input-loc)
+  (define input-file (open-input-file input-loc))
+  (define input (port->string input-file))
+  (close-input-port input-file)
+  (day-func input)
+  )
--- day1.rkt
+++ day1.rkt
@@ -4,44 +4,10 @@
 
 (provide day1 day1-exe)
 
-(define (day1 input_str)
-  (define input-port (open-input-string input_str))
-  (define elves '())
-  (define current_elf '())
-  (define (process-line)
-    (define line (read-line input-port))
-    (if
-     (eof-object? line)
-     (begin
-       (set! elves (cons current_elf elves))
-       (set! current_elf '())
-       )
-     (begin
-       (if
-        (equal? line "")
-        (begin
-          (set! elves (cons current_elf elves))
-          (set! current_elf '())
-          )
-        (let* ((num-port (open-input-string line)) (num (read num-port)))
-          (close-input-port num-port)
-          (if
-           (not (integer? num))
-           (error "A non number found in input")
-           (void)
-           )
-          (set! current_elf (cons num current_elf))
-          ))
-       (process-line))))
-  (process-line)
-  (close-input-port input-port)
-  (define elf-totals (sort (map (lambda (elf) (foldl + 0 elf)) elves) >))
-  `(,(car elf-totals) ,(+ (car elf-totals) (second elf-totals) (third elf-totals)))
+(define (day1 input-str)
+  (define elves (map (lambda [elf] (map string->number elf)) (block-split input-str)))
+  (define elf-totals (sort (map (lambda [elf] (foldl + 0 elf)) elves) >))
+  (list (first elf-totals) (apply + (take elf-totals 3)))
   )
 
-(define (day1-exe)
-  (define input-file (open-input-file "input/Day1.txt"))
-  (define input (port->string input-file))
-  (close-input-port input-file)
-  (day1 input)
-  )
+(define (day1-exe) (exe-day day1 "input/Day1.txt"))
